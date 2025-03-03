235d20687f3f5746d5a5b2dd5f5ffe369602bbae
Cristian A. Valencia P. <crivalpul@alu.edu.gva.es>
2025-03-03 17:52:12 +0100

    Arreglo en ProductoController

--- APP-ROLSER/app/Http/Controllers/Api/ProductoController.php
+++ APP-ROLSER/app/Http/Controllers/Api/ProductoController.php
@@ -16,9 +16,12 @@ class ProductoController extends Controller
     // Obtener un producto por ID
     public function show($id)
     {
-        $producto = Producto::with('almacen')->whereId($id);
+        // $producto = Producto::with('almacen')->whereId($id);
 
-        return response()->json($producto, 200);
+        // return response()->json($producto, 200);
+
+        $producto = Producto::findOrFail($id);
+        return $producto;
     }
 
     // Crear un nuevo producto
